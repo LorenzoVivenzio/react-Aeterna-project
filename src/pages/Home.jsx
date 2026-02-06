fdaae5ec02f2832ad4e5aa7722dc6d625edeae86
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-02-06 15:23:35 +0100

    feat: modifiche

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import CardHome from "../components/CardHome";
 import VideoBanner from "../components/VideoBanner";
 import axios from "axios";
-
+import Chat from "../components/Chat.jsx";
 export default function Home() {
   const [featured, setFeatured] = useState([]);
   const [created, setCreated] = useState([]);
@@ -35,12 +35,15 @@ export default function Home() {
           </p>
         </div>
         <div className="slogan">
-          <h1 className="text-slogan text-white">TOUCH WHAT <br /> TIME <br />ERASED</h1>
+          <h1 className="text-slogan text-white">
+            TOUCH WHAT <br /> TIME <br />
+            ERASED
+          </h1>
           <div className="btn-slogan">
             <button className="btn-btn-banner">
-              <a 
-              className="text-white"
-              href="#ultimi-arrivi">vai alle notività</a>
+              <a className="text-white" href="#ultimi-arrivi">
+                vai alle notività
+              </a>
             </button>
           </div>
         </div>
@@ -48,11 +51,8 @@ export default function Home() {
 
       <VideoBanner />
 
-      <div 
-      className="container my-5">
-        <section
-          id="ultimi-arrivi"
-          className="mb-5">
+      <div className="container my-5">
+        <section id="ultimi-arrivi" className="mb-5">
           <h1 className="mb-4">I nostri consigliati</h1>
           <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
             {featured.map((product) => (
@@ -65,9 +65,7 @@ export default function Home() {
 
         <hr />
 
-        <section
-
-          className="mt-5">
+        <section className="mt-5">
           <h1 className="mb-4">Ultimi arrivi</h1>
           <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
             {created.map((product) => (
@@ -75,6 +73,7 @@ export default function Home() {
                 <CardHome product={product} />
               </div>
             ))}
+            <Chat />
           </div>
         </section>
       </div>

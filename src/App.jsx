e73102a52bf1320bbf806a2deca4d478f156d3c5
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-02-10 17:00:38 +0100

    feat: add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,39 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import DefaultLayout from "./layout/DefaultLayout";
-
 import Home from "./pages/Home";
 import Products from "./pages/Products.jsx";
 import Wishlist from "./pages/Wishlist.jsx";
 import Checkout from "./pages/Checkout.jsx";
 import NotFound from "./pages/NotFound.jsx";
-
-
-
-import Home from "./pages/Home"
-import Products from "./pages/Products.jsx"
-import Wishlist from './pages/Wishlist.jsx';
-import Checkout from './pages/Checkout.jsx';
-
-
-import DetailProduct from './components/DetailProduct.jsx';
-import Cart from './pages/Cart.jsx';
-import About from './pages/About.jsx';
-import { WishlistProvider } from './context/WishlistContext.jsx';
+import DetailProduct from "./components/DetailProduct.jsx";
+import Cart from "./pages/Cart.jsx";
+import About from "./pages/About.jsx";
+import { WishlistProvider } from "./context/WishlistContext.jsx";
 import { CartProvider } from "./context/CartContext";
 
 function App() {
   return (
-    <CartProvider>
-      <WishlistProvider>    
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/product/:slug" element={<DetailProduct />} />
-            <Route path="/wishlist" element={<Wishlist />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-       </WishlistProvider>
-    </CartProvider>
-  )
+    // Spostiamo BrowserRouter all'esterno di tutto
+    <BrowserRouter>
+      <CartProvider>
+        <WishlistProvider>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/product/:slug" element={<DetailProduct />} />
+              <Route path="/wishlist" element={<Wishlist />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </WishlistProvider>
+      </CartProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;

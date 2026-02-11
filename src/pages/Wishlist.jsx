040ef2bb167ac45fe3ad2b1314c4c2ee84640e39
lucariggi92 <riggiluca92@gmail.com>
2026-02-11 12:44:18 +0100

    fix: resposive wishlist

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,23 +1,23 @@
 import { Link } from "react-router-dom";
 import { useWishlist } from "../context/WishlistContext";
-import {useCart}from "../context/CartContext";
+import { useCart } from "../context/CartContext";
 import "./Cart.css"
 
 export default function Wishlist() {
     const { wishlist, removeFromWishlist, clearWishlist } = useWishlist();
-       const { addToCart  } = useCart();
+    const { addToCart } = useCart();
 
     return (
         <>
 
-             <div className="cart-page  text-white min-vh-100" style={{ paddingTop: "120px" }}>
+            <div className="cart-page  text-white min-vh-100" style={{ paddingTop: "120px" }}>
                 <div className="container pb-5">
-                  <h1 className="display-5 fw-bold text-uppercase mb-5 cart-title">i tuoi preferiti</h1>
+                    <h1 className="display-5 fw-bold text-uppercase mb-5 cart-title">i tuoi preferiti</h1>
 
 
                     {wishlist.length === 0 ? (
                         <div className="text-center py-5">
-                           <p className="text-gray mb-4">La tua lista  dei Preferiti è vuota.</p>
+                            <p className="text-gray mb-4">La tua lista  dei Preferiti è vuota.</p>
                             <Link to="/products" className="btn-gold px-5 py-3 fw-bold">TORNA ALLO SHOP</Link>
                         </div>
                     ) : (
@@ -25,36 +25,39 @@ export default function Wishlist() {
                             <div className="col-lg-8">
                                 {wishlist.map((item) => (
 
+                                    <>
+                                        <div className="row-car">
+                                            <div key={item.slug} className="row-card d-flex align-items-center p-3 mb-3 shadow cart-border justify-content-between">
+                                                <div className="col-sm-card d-flex align-items-center">
+                                                    <img src={`http://localhost:3001/images/${item.url_image}`}
+                                                        alt={item.name}
+                                                        style={{ width: "90px", height: "90px", objectFit: "cover" }}
+                                                        className="rounded-3 border border-primary me-4"
+                                                    />
+                                                    <div className="flex-grow-1">
+                                                        <h2 className="mb-0 fw-bold">{item.name}</h2>
+                                                        <h3 className="mb-0 fw-bold" style={{ fontSize: "0.8rem" }}>{item.era}</h3>
+                                                        <h3 className="mb-0 fw-bold" style={{ fontSize: "0.8rem" }}>{item.diet}</h3>
 
-
-
-
-
-                                    <div key={item.slug} className="d-flex align-items-center bg-dark p-3 rounded-4 mb-3 border border-secondary shadow">
-                                        <img src={`http://localhost:3001/images/${item.url_image}`}
-                                            alt={item.name}
-                                            style={{ width: "90px", height: "90px", objectFit: "cover" }}
-                                            className="rounded-3 border border-primary me-4"
-                                        />
-                                        <div className="flex-grow-1">
-                                            <h2 className="mb-0 fw-bold">{item.name}</h2>
-                                            <h3 className="mb-0 fw-bold" style={{fontSize:"0.8rem"}}>{item.era}</h3>
-                                            <h3 className="mb-0 fw-bold" style={{fontSize:"0.8rem"}}>{item.diet}</h3>
-                                             
-                                            <p className="text-info mb-0 fw-semibold">{Number(item.price).toFixed(2)}€</p>
+                                                        <p className="text-info mb-0 fw-semibold">{Number(item.price).toFixed(2)}€</p>
+                                                    </div>
+                                                </div>
+                                                <div className="col-sm-card m-sm-15 d-flex align-items-center">
+                                                    <button onClick={() => addToCart(item)}
+                                                        className="btn-cart3">Aggiungi al carrello</button>
+                                                    <button onClick={() => removeFromWishlist(item.slug)}
+                                                        className="btn-svuota" style={{ padding: "10px", marginBottom: "0px" }}
+                                                        title="Rimuovi prodotto">
+                                                        Elimina
+                                                    </button>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <button onClick ={()=>addToCart(item.slug)}
-                                        className="btn-cart3">Aggiungi al carrello</button>
-                                        <button onClick={()=>removeFromWishlist(item.slug)}
-                                            className="btn-svuota" style={{padding:"10px", marginBottom:"0px"}}
-                                             title="Rimuovi prodotto">
-                                            Elimina
-                                        </button>
-                                    </div>
-                                                                       
+                                    </>
+
                                 ))}
-                                <button onClick={clearWishlist} 
-                                className="btn btn-outline-secondary btn-sm mt-3 opacity-75">
+                                <button onClick={clearWishlist}
+                                    className="btn btn-outline-secondary btn-sm mt-3 opacity-75">
                                     Svuota Lista Preferiti
                                 </button>
                             </div>
